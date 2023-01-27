a740f2a0e2c81d1de5458d176cb635ea3cf17788
riyan-eng <fbriyant11@gmail.com>
2023-01-27 11:45:03 +0700

    jwt

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+
+	jwtware "github.com/gofiber/jwt/v3"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func main() {
+	app := fiber.New()
+
+	// Login route
+	app.Post("/login", login)
+
+	// Unauthenticated route
+	app.Get("/", accessible)
+
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte("secret"),
+	}))
+
+	// Restricted Routes
+	app.Get("/restricted", restricted)
+
+	app.Listen(":3000")
+}
+
+type dtoLogin struct {
+	User     string `json:"user"`
+	Password string `json:"pass"`
+}
+
+func login(c *fiber.Ctx) error {
+	bodyjson := new(dtoLogin)
+	c.BodyParser(&bodyjson)
+
+	// Throws Unauthorized error
+	if bodyjson.User != "john" || bodyjson.Password != "doe" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	// Create the Claims
+	claims := jwt.MapClaims{
+		"name":  "John Doe",
+		"admin": true,
+		"exp":   time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	// Create token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token": t})
+}
+
+func accessible(c *fiber.Ctx) error {
+	return c.SendString("Accessible")
+}
+
+func restricted(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	fmt.Println(claims)
+	name := claims["name"].(string)
+	return c.SendString("Welcome " + name)
+}
